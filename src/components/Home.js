4361e69733089a7822b403884d874cfbc088b83e
Rotem10 <65648972+Rotem10@users.noreply.github.com>
2022-12-12 23:12:15 +0200

    router

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ export function Home() {
   return (
     <authContext.Provider value={appMethods}>
       <header>
-        <h2 className='mb-3'>Sign up to create your todo list</h2>
+        <h2 className='mb-3'>Sign in to create your todo list</h2>
         <Login />
       </header>
     </authContext.Provider>
